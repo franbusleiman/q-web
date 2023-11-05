c7e82aec1398a7599d40b713092d5c74c5ea1460
franbusleiman <franbusleiman@gmail.com>
2023-11-04 21:41:20 -0300

    first commit

---
+++ src/main/java/com/busleiman/qweb/QWebApplication.java
@@ -0,0 +1,13 @@
+package com.busleiman.qweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/busleiman/qweb/QWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busleiman.qweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
